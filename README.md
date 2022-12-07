794c429ec15fb18b25566d98e9b0e9b876c95c98
VikasVashisth <vikas.vashisth@knoldus.com>
2022-12-07 15:43:57 +0530

    Added files , initial commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+# DESCRIPTION
+
+Here in this template, you will learn about the concept of creating ECS task Definition and DeRegister it. 
+
+---
+
+#### Prerequiste
+
+* Install Python
+* AWS Configured
+
+### Steps 
+
+1. Open the folder in your terminal or any supported tool.
+2. Run the following commands:
+
+`python3 create-ecs-task-definition.py`
+
+---
+
+### Output  
+
+![chromeversion.png](assests/chromeversion.png)
+
+![](assests/create-ecs-task-definition.png)
+
+---
---
+++ create-ecs-task-definition-boto3.py
@@ -0,0 +1,36 @@
+import boto3
+import json
+
+client = boto3.client("ecs", region_name="us-east-1")
+
+response = client.register_task_definition(
+    containerDefinitions=[
+        {
+            "name": "AmazonSampleImage",
+            "image": "amazon/amazon-ecs-sample",
+            "cpu": 0,
+            "portMappings": [],
+            "essential": True,
+            "environment": [],
+            "mountPoints": [],
+            "volumesFrom": [],
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-group": "/ecs/AWSSampleApp",
+                    "awslogs-region": "us-east-1",
+                    "awslogs-stream-prefix": "ecs"
+                }
+            }
+        }
+    ],
+    executionRoleArn="arn:aws:iam::585584209241:role/ecsTaskExecutionRole",
+    family= "AWSSampleApp2",
+    networkMode="awsvpc",
+    requiresCompatibilities= [
+        "FARGATE"
+    ],
+    cpu= "256",
+    memory= "512")
+
+print(json.dumps(response, indent=4, default=str))
